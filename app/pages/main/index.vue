b706c4d5c7f405d4cf772c37263f04143b715d39
Danil Kolbasenko <danilvusockij@gmail.com>
2026-09-09 12:19:24 +0300

    Compalted second block

--- app/pages/main/index.vue
+++ app/pages/main/index.vue
@@ -29,7 +29,52 @@
             </nav>
 
         </div>
-
+        <div class="py-3"">
+            <hr>
+        </div>
     </header>
+    <div class=" flex ">
+
+        <div class=" px-20 ">
+            <p class=" text-2xl ">Woman’s Fashion</p>
+            <hr>
+            <p class=" text-2xl py-2">Men’s Fashion</p>
+            <hr>
+            <p class="text-2xl ">Electronics</p>
+            <hr>
+            <p class="text-2xl py-2 ">Home & Lifestyle</p>
+            <hr>
+            <p class="text-2xl ">Medicine</p>
+            <hr>
+            <p class="text-2xl py-2">Sports & Outdoor</p>
+            <hr>
+            <p class="text-2xl ">Baby’s & Toys</p>
+            <hr>
+            <p class="text-2xl py-2 ">Groceries & Pets</p>
+            <hr>
+            <p class="text-2xl ">Health & Beauty</p>
+        </div>
+
+        <div class="flex justify-end px-30 ">
+            <div class="w-[70rem]  bg-black h-130 rounded-2xl">
+                <div class="grid grid-cols-2 gap-4">
+                    <div class="py-25 px-25">
+                        <div class="flex  ">
+                            <img class="w-13" src="/apple.svg" alt="">
+                            <p class="text-white py-3 px-5 text-3xl ">Iphone 14 Series </p>
+                        </div>
+                        <p class=" py-3 text-7xl text-white">Up to 10% off Voucher</p>
+                        <NuxtLink class="text-2xl py-2 text-white " to="/main">Shop now </NuxtLink>
+                    </div>
+                    <div class="flex justify-end py-25">
+                        <img class="w-[40rem]" src="/add.svg" alt="">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        </div>
+
+
 </template>
 <script setup lang='ts'></script>
