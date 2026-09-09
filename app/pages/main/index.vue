17c810c02f5e66e51b606435a6aafd69d93ebfa4
Danil Kolbasenko <danilvusockij@gmail.com>
2026-09-09 11:42:47 +0300

    Succes Header

--- app/pages/main/index.vue
+++ app/pages/main/index.vue
@@ -1,4 +1,35 @@
 <template>
-    <h1 class="">This a new project </h1>
+    <div class="w-full bg-black h-15">
+        <div>
+            <p class="text-white text-xl text-center py-5">Summer Sale For all Swim Suits And Free Express Delivery -
+                OFF 50
+                %! ShopNow
+            </p>
+        </div>
+    </div>
+
+    <header class="px-30 py-15">
+        <div class="">
+            <div>
+            </div>
+            <nav class="flex justify-between ">
+                <p class="text-4xl font-bold ">Exclusive</p>
+
+                <NuxtLink class="text-2xl py-2" to="/main">Home</NuxtLink>
+                <NuxtLink class="text-2xl py-2" to="">Contact</NuxtLink>
+                <NuxtLink class="text-2xl py-2" to="">About</NuxtLink>
+                <NuxtLink class="text-2xl py-2" to="">sign Up </NuxtLink>
+                <input class="w-80 h-10 border rounded-2xl mt-2 p-3 bg-gray-100" placeholder="What are you looking for?"
+                    type="text">
+                <div class="flex py-2">
+                    <img class="w-10" src="/Cart1.svg" alt="">
+                    <img class="ml-8 w-10" src="/Wishlist.svg" alt="">
+                </div>
+
+            </nav>
+
+        </div>
+
+    </header>
 </template>
 <script setup lang='ts'></script>
